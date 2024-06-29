f3fb1870fa1fd29905be58b6ed5654eb32f15dc7
Kishor Markad <markadk@gmail.com>
2024-06-29 14:58:42 +0530

    Testing

---
+++ src/practice/MaxBet_2.java
@@ -0,0 +1,18 @@
+package practice;
+
+public class MaxBet_2 {
+
+	public static void main(String[] args) {
+		int a=10,b=20;
+		if(a<b)
+		{
+			System.out.println("A is smaller");
+		}
+		else
+		{
+			System.out.println("A is Bigger");
+		}
+
+	}
+
+}
